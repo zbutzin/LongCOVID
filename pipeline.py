9419ea4e368d8b25bae53dce475131c80cfdcb2c
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2024-11-27 18:34:13 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1,2 +1,10 @@
 
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.3157281f-4709-46bd-918a-df9b0048a7a6")
+)
+from pyspark.sql.types import *
+def unnamed():
+    schema = StructType([])
+    return spark.createDataFrame([[]], schema=schema)
+
