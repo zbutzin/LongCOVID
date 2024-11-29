db8cf75531f482d06340a7169a6b0a67cb8eca4f
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2024-11-29 17:18:33 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -286,6 +286,54 @@ from pyspark.sql import functions as F
     
 
 @transform_pandas(
+    Output(rid="ri.foundry.main.dataset.06da3b80-8298-4fb1-8db7-ee493c1fcfe5"),
+    concept_set_members=Input(rid="ri.foundry.main.dataset.e670c5ad-42ca-46a2-ae55-e917e3e161b6"),
+    drug_exposure=Input(rid="ri.foundry.main.dataset.ec252b05-8f82-4f7f-a227-b3bb9bc578ef")
+)
+# everyone_drugs_of_interest (ef43cf2e-5442-4ad8-924b-27e1d7ca17f7): v4
+#Purpose - The purpose of this pipeline is to produce a day level and a persons level fact table for all patients in the N3C enclave.
+#Creator/Owner/contact - Andrea Zhou
+#Last Update - 12/7/22
+#Description - This nodes filter the source OMOP tables for rows that have a standard concept id associated with one of the concept sets described in the data dictionary in the README through the use of a fusion sheet.  Indicator names for these variables are assigned, and the indicators are collapsed to unique instances on the basis of patient and date.
+
+def everyone_drugs_of_interest(concept_set_members, drug_exposure, , ):
+  
+    #bring in only cohort patient ids
+    persons = .select('person_id')
+    #filter drug exposure table to only cohort patients    
+    drug_df = drug_exposure \
+        .select('person_id','drug_exposure_start_date','drug_concept_id') \
+        .where(F.col('drug_exposure_start_date').isNotNull()) \
+        .withColumnRenamed('drug_exposure_start_date','date') \
+        .withColumnRenamed('drug_concept_id','concept_id') \
+        .join(persons,'person_id','inner')
+
+    #filter fusion sheet for concept sets and their future variable names that have concepts in the drug domain
+    fusion_df =  \
+        .filter(.domain.contains('drug')) \
+        .select('concept_set_name','indicator_prefix')
+    #filter concept set members table to only concept ids for the drugs of interest
+    concepts_df = concept_set_members \
+        .select('concept_set_name', 'is_most_recent_version', 'concept_id') \
+        .where(F.col('is_most_recent_version')=='true') \
+        .join(fusion_df, 'concept_set_name', 'inner') \
+        .select('concept_id','indicator_prefix')
+        
+    #find drug exposure information based on matching concept ids for drugs of interest
+    df = drug_df.join(concepts_df, 'concept_id', 'inner')
+    #collapse to unique person and visit date and pivot on future variable name to create flag for rows associated with the concept sets for drugs of interest
+    df = df.groupby('person_id','date').pivot('indicator_prefix').agg(F.lit(1)).na.fill(0)
+
+    return df
+    
+
+#################################################
+## Global imports and functions included below ##
+#################################################
+
+from pyspark.sql import functions as F
+
+@transform_pandas(
     Output(rid="ri.foundry.main.dataset.08da4c9c-b5d9-43ff-b638-76f1be5655bf"),
     concept_set_members=Input(rid="ri.foundry.main.dataset.e670c5ad-42ca-46a2-ae55-e917e3e161b6"),
     customized_concept_set_input=Input(rid="ri.foundry.main.dataset.da40e627-9c72-416b-8cb6-8d13d6595dee"),
