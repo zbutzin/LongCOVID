62351081af790da0149bdb1412e39ddc4377d6f4
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2024-12-11 18:09:57 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -12,3 +12,10 @@ FROM Final_table_2 AS ft
 JOIN all_patients_fact_day_table_LDS AS ap
 ON ft.new_person_id = ap.person_id
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.8d51362c-5fc3-4076-93de-e069e19877c3"),
+    Join_1=Input(rid="ri.foundry.main.dataset.6e355892-de04-497a-8a7b-a323d7e56b76")
+)
+SELECT *
+FROM Join_1
+
