ca3b555b1b70b3d3d4d060d4980700eef9d75e64
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2024-12-11 17:55:34 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -1,2 +1,9 @@
 
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.d5e25150-8bb8-4682-a067-6f3442e1bf56"),
+    Final_table_2=Input(rid="ri.foundry.main.dataset.e3b48760-d3ac-410b-a780-e00f393ff0f5")
+)
+SELECT *
+FROM Final_table_2
+
