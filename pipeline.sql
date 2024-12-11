2721fe27291f53dba8cdeb44bf9f0e6a2fc4bfc7
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2024-12-11 18:10:38 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -13,9 +13,10 @@ JOIN all_patients_fact_day_table_LDS AS ap
 ON ft.new_person_id = ap.person_id
 
 @transform_pandas(
-    Output(rid="ri.vector.main.execute.8d51362c-5fc3-4076-93de-e069e19877c3"),
+    Output(rid="ri.foundry.main.dataset.71aac910-05e4-455a-9303-72d94e5d8be0"),
     Join_1=Input(rid="ri.foundry.main.dataset.6e355892-de04-497a-8a7b-a323d7e56b76")
 )
 SELECT *
 FROM Join_1
+WHERE date > drug_exposure_start_date;
 
