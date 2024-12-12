4e50f5a647cd48aced85701ff5d85ff3f2902ffb
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2024-12-12 00:14:24 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -2,13 +2,13 @@
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.6e355892-de04-497a-8a7b-a323d7e56b76"),
-    Final_table_2=Input(rid="ri.foundry.main.dataset.e3b48760-d3ac-410b-a780-e00f393ff0f5"),
-    all_patients_fact_day_table_LDS=Input(rid="ri.foundry.main.dataset.5c331e73-d93a-4316-922e-82b4d06b1131")
+    all_patients_fact_day_table_LDS=Input(rid="ri.foundry.main.dataset.5c331e73-d93a-4316-922e-82b4d06b1131"),
+    rename_1=Input(rid="ri.foundry.main.dataset.c55947ba-e74b-4027-acb2-0a3bb56908d0")
 )
 SELECT 
     ft.*, 
     ap.*
-FROM Final_table_2 AS ft
+FROM rename_1 AS ft
 JOIN all_patients_fact_day_table_LDS AS ap
 ON ft.new_person_id = ap.person_id
 
