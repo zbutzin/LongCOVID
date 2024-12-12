4bc5cd17af40224d96a0869c55395f7ec7cd7d86
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2024-12-12 15:36:24 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -25,19 +25,19 @@ WHERE date > drug_exposure_start_date;
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.fa71db87-90dd-4dc4-a14c-c3dd8bbe7475"),
-    unnamed_1=Input(rid="ri.foundry.main.dataset.71aac910-05e4-455a-9303-72d94e5d8be0")
+    Join_1=Input(rid="ri.foundry.main.dataset.6e355892-de04-497a-8a7b-a323d7e56b76")
 )
 WITH covid_dates AS (
     -- Get all COVID diagnosis dates for each person
     SELECT DISTINCT
         person_id,
-        unnamed_1.date as covid_date
-    FROM unnamed_1
+        Join_1.date as covid_date
+    FROM Join_1
     WHERE confirmed_covid_patient = 1
 )
 
 SELECT DISTINCT t.*
-FROM unnamed_1 t
+FROM Join_1 t
 LEFT JOIN covid_dates c
     ON t.person_id = c.person_id
 WHERE 
