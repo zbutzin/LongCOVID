e529d2c269088249be40aaf9f7dcd6bf3f339b1f
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2025-01-16 18:24:39 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -57,15 +57,3 @@ ORDER BY date DESC;
 -- confirm this logic with Zach, it might be the other way around. 
 -- how do i make this more similar to all_patients table with the logic? I need four inputs but idk how to input them like that 
 
-@transform_pandas(
-    Output(rid="ri.foundry.main.dataset.cb95ba7e-7f11-4d96-91fa-78c7cf21eccc"),
-    all_patients_fact_day_table_LDS=Input(rid="ri.foundry.main.dataset.5c331e73-d93a-4316-922e-82b4d06b1131"),
-    rename_1=Input(rid="ri.foundry.main.dataset.c55947ba-e74b-4027-acb2-0a3bb56908d0")
-)
-SELECT 
-    ft.*, 
-    ap.*
-FROM rename_1 AS ft
-JOIN all_patients_fact_day_table_LDS AS ap
-ON ft.new_person_id = ap.person_id
-
