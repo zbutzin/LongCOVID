4a4ea2d87748a2a7865f4d0807d55c39d281d827
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2025-02-21 07:50:45 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -33,9 +33,13 @@ FROM condition_occurrence_1
 where condition_concept_id in (705076, 710706)
 
 @transform_pandas(
-    Output(rid="ri.vector.main.execute.1e4c1b38-fbca-4449-a620-e167c7889b32"),
-    rename_2=Input(rid="ri.foundry.main.dataset.d9fd0d20-392f-4c78-91dd-b93afeaedca5")
+    Output(rid="ri.foundry.main.dataset.817258a4-e831-49b2-be2d-1078b061a881"),
+    rename_2=Input(rid="ri.foundry.main.dataset.d9fd0d20-392f-4c78-91dd-b93afeaedca5"),
+    rename_covid=Input(rid="ri.foundry.main.dataset.75b7b90a-aa84-4385-bef8-5a2184a82ea2")
 )
-SELECT *
-FROM rename_2
+SELECT ft.*, 
+       rc.*
+FROM rename_2 as ft
+JOIN rename_covid as rc
+    ON ft.new_person_id = rc.covid_date_person_id; 
 
