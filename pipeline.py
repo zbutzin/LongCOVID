faa57a8a974c61ba43ef82fe90ae9d96d5437910
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2025-02-21 08:19:01 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -889,7 +889,7 @@ def unnamed_2(unnamed_1):
     unnamed_2=Input(rid="ri.foundry.main.dataset.d1d219fa-ab7a-433b-a5d1-fa97c069e027")
 )
 def unnamed_3(unnamed_2):
-    #df of patients to drop from final table 
+    #df of patients to drop from final table
     filtered_df = unnamed_2[unnamed_2['long_covid_date'] <= unnamed_2['drug_exposure_start_date']]
     
     return filtered_df
@@ -915,6 +915,13 @@ def unnamed_4(unnamed_3):
 
 def unnamed_5(rename_covid, unnamed_4):
      return rename_covid[~rename_covid["covid_date_person_id"].isin(unnamed_4["person_id"])]
+
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.1427c678-fe8e-44ad-8c35-f31d939293dd"),
+    unnamed_1=Input(rid="ri.foundry.main.dataset.817258a4-e831-49b2-be2d-1078b061a881")
+)
+def unnamed_7(unnamed_1):
+    
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.ac3ab840-7c81-4082-a322-beb71227c40b"),
