a7d84f03a572ac09edd2f976e72bfea7223b5a2f
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2025-02-21 07:53:31 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -868,6 +868,13 @@ def unnamed():
     return spark.createDataFrame([[]], schema=schema)
 
 @transform_pandas(
+    Output(rid="ri.vector.main.execute.347a2d32-eb7b-48c2-879c-f22a0d092754"),
+    unnamed_1=Input(rid="ri.foundry.main.dataset.817258a4-e831-49b2-be2d-1078b061a881")
+)
+def unnamed_2(unnamed_1):
+    
+
+@transform_pandas(
     Output(rid="ri.foundry.main.dataset.ac3ab840-7c81-4082-a322-beb71227c40b"),
     filter_covid_dates=Input(rid="ri.foundry.main.dataset.94dce3b8-b337-45ae-87f2-40661cb5e181")
 )
